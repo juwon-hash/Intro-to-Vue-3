b94f7a36e03d3465cd003ea59e1c6144b077125c
Jayce <94034494+treenod-jayce@users.noreply.github.com>
2022-05-17 18:04:47 +0900

    vue section 1~6

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-const product = 'Socks'
+//const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            inventory: 9,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ],
+            cart: 0,
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
+    }
+})
